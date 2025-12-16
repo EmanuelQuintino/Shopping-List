487066f1e3bd8827299ac9fc81d23fc61f38fe81
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-12-16 00:41:22 -0300

    add component button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,14 @@
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { styles } from "./styles";
+
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button} activeOpacity={0.8} {...rest}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
---
+++ src/components/button/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2c46b1",
+    height: 48,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+  },
+
+  text: {
+    color: "#FFFFFF",
+    fontSize: 14,
+    fontWeight: 600,
+    textAlign: "center",
+  },
+});
