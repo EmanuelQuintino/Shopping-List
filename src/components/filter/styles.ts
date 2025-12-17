5dd5b94f6215663da99ff589c7a9d380583a81c9
Emanuel Quintino <emanuelquintino@hotmail.com>
2025-12-17 00:06:10 -0300

    add status icon to filter

---
+++ src/components/filter/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  filterButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 600,
+  },
+});
